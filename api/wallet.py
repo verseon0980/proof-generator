87fd87746818476e792676abf13af767c188e518
verseon0980 <klokrc74@gmail.com>
2026-03-29 16:19:35 +0530

    Update wallet.py

--- api/wallet.py
+++ api/wallet.py
@@ -1,9 +1,25 @@
-from eth_account import Account
 import os
+import json
+from eth_account import Account
+from http.server import BaseHTTPRequestHandler
 
-def handler(req, res):
-    pk = os.environ.get("OG_PRIVATE_KEY", "")
-    if not pk:
-        return res.status(500).json({"error": "OG_PRIVATE_KEY not set"})
-    wallet = Account.from_key(pk)
-    return res.status(200).json({"address": wallet.address})
+
+class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        pk = os.environ.get("OG_PRIVATE_KEY", "")
+        if not pk:
+            self._json(500, {"error": "OG_PRIVATE_KEY not set"})
+            return
+        wallet = Account.from_key(pk)
+        self._json(200, {"address": wallet.address})
+
+    def _json(self, code, data):
+        payload = json.dumps(data).encode()
+        self.send_response(code)
+        self.send_header("Content-Type", "application/json")
+        self.send_header("Content-Length", len(payload))
+        self.end_headers()
+        self.wfile.write(payload)
+
+    def log_message(self, *args):
+        pass
